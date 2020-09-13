eb03e1dd65b168ff2e2ac78a1b1ed3d0b072c5f3
visualsoul <zbynek.vrzalik@gmail.com>
2020-09-14 00:39:40 +0100

    Updated

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("random/", views.random_entry, name="random_entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
-
+from django.urls import reverse
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from . import util
+
+from markdown2 import Markdown
+import random
+md = Markdown()
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    if entry is not None:
+        html_data = md.convert(entry)
+        return render(request, "encyclopedia/entry.html", {
+        'title': title, 'html_data': html_data
+        })
+    else:
+        raise Http404(f"404 Error - Entry ({title}) doesn't exist!")
+
+
+def random_entry(request):
+    entries = util.list_entries()
+    if len(entries) > 0:
+        random.shuffle(entries)
+        title = entries.pop()
+        entry = util.get_entry(title)
+        if entry is not None:
+            html_data = md.convert(entry)
+            return render(request, "encyclopedia/entry.html", {
+                'title': title, 'html_data': html_data
+                })
+    return render(request, "encyclopedia/error.html")
 
